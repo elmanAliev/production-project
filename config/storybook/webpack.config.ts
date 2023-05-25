85b02ac8cdbf80e9bc23aaecc87a585300c8ff46
Elman Aliev <elman3605@yandex.ru>
2023-05-25 20:24:51 +0300

    88 fix storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -17,6 +17,10 @@ export default ({ config }: {config: webpack.Configuration}) => {
     config.resolve!.modules = [paths.src, "node_modules"];
 
     config.resolve!.extensions!.push('.ts', '.tsx');
+    config.resolve!.alias = {
+        ...config.resolve!.alias,
+        "@": paths.src,
+    };
 
     // eslint-disable-next-line no-param-reassign
     config.module!.rules = config.module!.rules!.map((rule: any) => {
